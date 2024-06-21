568df86cc3b31a0ed82148a4dbc38ddfacab8e46
D.J. Rideout <djrideoutgit@proton.me>
2024-06-21 16:28:10 -0230

    Write RSS to file

--- index.mjs
+++ index.mjs
@@ -1,3 +1,4 @@
+import fs from "node:fs/promises";
 import jsdom from "jsdom";
 import { Article, Feed } from "./rss.mjs";
 
@@ -7,4 +8,5 @@ global.DOMParser = new (new JSDOM()).window.DOMParser();
 let xml = await fetch("https://vocm.com/feed/").then(v => v.text());
 let parsed = DOMParser.parseFromString(xml, "application/xml");
 let feed = Feed.fromElement(parsed);
-console.log(feed.toString());
+await fs.mkdir("gh-pages", { recursive: true });
+await fs.writeFile("gh-pages/rss.xml", feed.toString());
